d3cf6e12f163ea1a563d546715b3b2fdff4970b1
pauleveritt <paul.everitt@jetbrains.com>
2023-08-27 10:12:59 -0400

    Step 3. Make a reproducible failure (and remove Vite, to simplify.)

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,13 @@
-const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
-
 module.exports = function (eleventyConfig) {
-  // v2.0.0-canary.19 or newer
   eleventyConfig.addExtension(["11ty.jsx", "11ty.ts", "11ty.tsx"], {
     key: "11ty.js",
   });
-  eleventyConfig.addPlugin(EleventyVitePlugin);
 
   return {
     dir: {
       input: "site",
-      // output: "../_site",
-      layouts: "../_layouts",
     },
     markdownTemplateEngine: "11ty.js",
+    pathPrefix: "/testdir",
   };
 };
--- site/index.test.tsx
+++ site/index.test.tsx
@@ -1,8 +1,20 @@
 import { expect, test } from "vitest";
 
-import { render } from "./index.11ty";
+const Eleventy = require("../node_modules/@11ty/eleventy/src/Eleventy.js");
 
-test("render index", () => {
-  const result = render();
-  expect(result).to.contain("The body");
+test("render Eleventy", async () => {
+  const elev = new Eleventy("..", "../_site");
+  const results = await elev.toJSON();
+  expect(results).to.be.true;
 });
+
+/*
+
+The constructor leads to this error in line 331 in TemplateConfig.js:
+
+require() of ES Module /Users/pauleveritt/projects/pauleveritt/eleventy-vitest/eleventy.config.js from /Users/pauleveritt/projects/pauleveritt/eleventy-vitest/node_modules/@11ty/eleventy/src/Util/Require.js not supported.
+eleventy.config.js is treated as an ES module file as it is a .js file whose nearest parent package.json contains "type": "module" which declares all .js files in that package scope as ES modules.
+Instead rename eleventy.config.js to end in .cjs, change the requiring code to use dynamic import() which is available in all CommonJS modules, or change "type": "module" to "type": "commonjs" in /Users/pauleveritt/projects/pauleveritt/eleventy-vitest/package.json to treat all .js files as CommonJS (using .mjs for all ES modules instead).
+
+
+ */
